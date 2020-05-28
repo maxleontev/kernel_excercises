9c69fc9ae4d0032f095d2320e81af4173634fbad
Max Leontev <_leon_@inbox.ru>
2020-05-28 14:46:58 +0800

    example from chapter 03 with a dynamic node creation

--- solution.c
+++ solution.c
@@ -15,6 +15,8 @@ static struct cdev *maxs_cdev;
 #define MAX_DEV_NAME "maxchrdev"
 #define KBUF_SIZE (size_t) 10*PAGE_SIZE
 
+static struct class *maxs_class;
+
 static int max_chr_drv_open(struct inode *inode, struct file *file) {
 	static int counter = 0;
 	char *kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
@@ -58,36 +60,73 @@ static ssize_t max_chr_drv_write(struct file *file, const char __user *buf, size
 	return nbytes;
 }
 
+static loff_t max_chr_drv_lseek (struct file *file, loff_t offset, int orig) {
+    loff_t testpos;
+
+    switch (orig) {
+        case SEEK_SET :
+            testpos = offset;
+            break;
+        case SEEK_CUR : 
+            testpos = file->f_pos + offset;
+            break;
+        case SEEK_END : 
+            testpos = KBUF_SIZE + offset;
+            break;
+        default:
+            return -EINVAL;
+            break;
+    }
+
+    if (testpos >= KBUF_SIZE) testpos = KBUF_SIZE - 1;
+    if (testpos < 0) testpos = 0;
+    file->f_pos = testpos;
+
+    printk ( KERN_INFO "Seeking to %ld position\n", (long)testpos);
+
+    return testpos;
+}
+
 static const struct file_operations maxs_cdev_fops = {
 	.owner = THIS_MODULE,
 	.read = max_chr_drv_read,
 	.write = max_chr_drv_write,
 	.open = max_chr_drv_open,
-	.release = max_chr_drv_release
+	.release = max_chr_drv_release,
+    .llseek = max_chr_drv_lseek
 };
 
-static int __init init_mod(void) {
+static int __init init_module_chrdrv(void) {
 	printk( KERN_INFO "Hello, solution module loaded!\n" );
 
 	first = MKDEV (max_chr_drv_major, max_chr_drv_minor);
-	register_chrdev_region(first, count, MAX_DEV_NAME);
+	register_chrdev_region (first, count, MAX_DEV_NAME);
 
 	maxs_cdev = cdev_alloc();
 
-	cdev_init(maxs_cdev, &maxs_cdev_fops);
-	cdev_add(maxs_cdev, first, count);
+	cdev_init (maxs_cdev, &maxs_cdev_fops);
+	cdev_add (maxs_cdev, first, count);
+
+    maxs_class = class_create (THIS_MODULE, "maxs_class");
+    device_create (maxs_class, NULL, first, "%s", "chrdrv");
+
+    printk ( KERN_INFO "Created device class :%s\n", MAX_DEV_NAME);
 
 	return 0;
 }
 
-static void __exit exit_mod(void) {
+static void __exit exit_module_chrdrv(void) {
+
+    device_destroy (maxs_class, first);
+    class_destroy (maxs_class);
+
 	if (maxs_cdev)
 		cdev_del (maxs_cdev);
 	unregister_chrdev_region(first, count);
 	printk( KERN_INFO "Hello, solution module leaved!\n" );
 }
 
-module_init(init_mod);
-module_exit(exit_mod);
+module_init(init_module_chrdrv);
+module_exit(exit_module_chrdrv);
 
 MODULE_LICENSE("GPL");
