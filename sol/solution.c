212b5ecb85ea8a301ba51340d5e9269755e46929
Max Leontev <_leon_@inbox.ru>
2020-07-06 14:31:55 +0800

    Solution for the task 01 of chapter 03 (dynamic nodes) You can create dynamic node "chrdrv" (or you could set this name at insmod parameter 'node_name') with a dynamic major number and you could read this major number from this node. also you can change the rules for this node using .rules file, it needs to read data form the node without 'sudo'.

--- sol/solution.c
+++ sol/solution.c
@@ -1,46 +1,123 @@
-#include <linux/module.h>
-#include <linux/device.h>
 #include <linux/slab.h>
-
-#include "checker.h"
-
-extern ssize_t get_void_size(void);
-extern void submit_void_ptr(void *p);
-extern ssize_t get_int_array_size(void);
-extern void submit_int_array_ptr(int *p);
-extern void submit_struct_ptr(struct device *p);
-extern void checker_kfree(void *p);
+#include <linux/device.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 
 
-void * void_ptr;
-int * int_arr_ptr;
-struct device * struct_device_ptr;
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Max Leontev"); 
+MODULE_DESCRIPTION("Linux char driver for the modules development course");
+MODULE_VERSION("0.1");
 
-static int __init init_solution(void) {
-//	printk( KERN_INFO "Hello, solution module loaded!\n" );
+static int max_chr_drv_major_number;
 
-    void_ptr = kmalloc(get_void_size(), GFP_ATOMIC);
-    submit_void_ptr(void_ptr);
+static struct class* max_chr_drv_class = NULL; 	// The device-driver class struct pointer
+static struct device* max_chr_drv_device = NULL;// The device-driver device struct pointer
 
-    int_arr_ptr = kmalloc(get_int_array_size()*sizeof(int), GFP_ATOMIC);
-    submit_int_array_ptr(int_arr_ptr);
+#define MAX_CLASS_NAME  "maxchrdev"				// The device class -- this is a character device driver
 
-    struct_device_ptr = kmalloc(sizeof(struct device), GFP_ATOMIC);
-    submit_struct_ptr(struct_device_ptr);
+#define KBUF_SIZE (size_t) 10*PAGE_SIZE
+static char *kbuf;								//define main single buffer for all 
+static char *msg_ptr;
+
+static char *node_name = "chrdrv";
+
+module_param(node_name, charp, S_IRUGO);
+
+static int max_chr_drv_open(struct inode *inode, struct file *file) {
+	msg_ptr = kbuf;
+	printk( KERN_INFO "Solution module: Opening device: %s \n", MAX_CLASS_NAME);
+	return 0;
+}
+
+static int max_chr_drv_release(struct inode *inode, struct file *file) {
+	printk( KERN_INFO "Solution module: Releasing device: %s \n", MAX_CLASS_NAME);
+	return 0;
+}
+
+static ssize_t max_chr_drv_read(struct file *file, char __user *buf, size_t lbuf, loff_t *ppos) {
+	int nbytes = 0;
+
+	sprintf(kbuf, "%d\n", max_chr_drv_major_number);
+
+	if (*msg_ptr == 0) return 0;
+	while ( lbuf && *msg_ptr ) {
+		put_user(*(msg_ptr++), buf++);
+		lbuf--;
+    	nbytes++;
+	}
+	*ppos += nbytes;
+	printk( KERN_INFO "Solution module: Reading device: %s, bytes readed = %d, ppos = %d\n", MAX_CLASS_NAME, nbytes, (int) *ppos );
+	return nbytes;
+}
+
+static ssize_t max_chr_drv_write(struct file *file, const char __user *buf, size_t lbuf, loff_t *ppos) {
+	int nbytes = lbuf - copy_from_user(kbuf + *ppos, buf, lbuf);
+	*ppos += nbytes;
+
+	printk( KERN_INFO "Solution module: Writing device: %s, bytes writed = %d, ppos = %d\n", MAX_CLASS_NAME, nbytes, (int) *ppos );
+	return nbytes;
+}
+
+static const struct file_operations maxs_cdev_fops = {
+	.owner = THIS_MODULE,
+	.read = max_chr_drv_read,
+	.write = max_chr_drv_write,
+	.open = max_chr_drv_open,
+	.release = max_chr_drv_release
+};
+
+static int __init init_mod(void) {
+
+	printk( KERN_INFO "Solution module: Loaded!\n" );
+
+	// Try to dynamically allocate a major number for the device -- more difficult but worth it
+	max_chr_drv_major_number = register_chrdev(0, node_name, &maxs_cdev_fops);
+	if (max_chr_drv_major_number < 0) {
+		printk(KERN_ALERT "Solution module: failed to register a major number\n");
+		return max_chr_drv_major_number;
+	}
+	
+	printk(KERN_INFO "Solution module: registered major number %d\n", max_chr_drv_major_number);
+	
+	// Register the device class
+	max_chr_drv_class = class_create(THIS_MODULE, MAX_CLASS_NAME);
+	if (IS_ERR(max_chr_drv_class)) {										// Check for error and clean up if there is
+		unregister_chrdev(max_chr_drv_major_number, node_name);
+		printk(KERN_ALERT "Solution module: Failed to register device class\n");
+		return PTR_ERR(max_chr_drv_class);									// Correct way to return an error on a pointer
+	}
+	printk(KERN_INFO "Solution module: device class registered correctly\n");
+
+	// Register the device driver
+	max_chr_drv_device = device_create(max_chr_drv_class, NULL, MKDEV(max_chr_drv_major_number, 0), NULL, node_name);
+	if (IS_ERR(max_chr_drv_device)) {										// Clean up if there is an error
+		class_destroy(max_chr_drv_class); 									// Repeated code but the alternative is goto statements
+		unregister_chrdev(max_chr_drv_major_number, node_name);
+		printk(KERN_ALERT "Solution module: Failed to create the device\n");
+		return PTR_ERR(max_chr_drv_device);
+	}
+	printk(KERN_INFO "Solution module: device class created correctly\n"); // Made it! device was initialized
+
+	kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
 
 	return 0;
 }
 
-static void __exit exit_solution(void) {
-    
-    checker_kfree(void_ptr);
-    checker_kfree(int_arr_ptr);
-    checker_kfree(struct_device_ptr);
+static void __exit exit_mod(void) {
 
-//	printk( KERN_INFO "Hello, solution module leaved!\n" );
+	if (kbuf) kfree(kbuf);
+
+	device_destroy(max_chr_drv_class, MKDEV(max_chr_drv_major_number, 0));	// remove the device
+	class_unregister(max_chr_drv_class);									// unregister the device class
+	class_destroy(max_chr_drv_class);										// remove the device class
+	unregister_chrdev(max_chr_drv_major_number, node_name);			// unregister the major number
+
+	printk( KERN_INFO "Solution module: Leaved!\n" );
 }
 
-module_init(init_solution);
-module_exit(exit_solution);
-
-MODULE_LICENSE("GPL");
+module_init(init_mod);
+module_exit(exit_mod);
--- solution.c
+++ solution.c
@@ -1,90 +1,65 @@
+#include <linux/slab.h>
+#include <linux/device.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
-#include <linux/init.h>
-#include <linux/slab.h>
-#include <linux/cdev.h>
 
 
-static dev_t first;
-static unsigned int count = 1;
-static int max_chr_drv_major = 500;
-static int max_chr_drv_minor = 0;
-static struct cdev *maxs_cdev;
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Max Leontev"); 
+MODULE_DESCRIPTION("Linux char driver for the modules development course");
+MODULE_VERSION("0.1");
 
-#define MAX_DEV_NAME "maxchrdev"
+static int max_chr_drv_major_number;
+
+static struct class* max_chr_drv_class = NULL; 	// The device-driver class struct pointer
+static struct device* max_chr_drv_device = NULL;// The device-driver device struct pointer
+
+#define MAX_CLASS_NAME  "maxchrdev"				// The device class -- this is a character device driver
+
 #define KBUF_SIZE (size_t) 10*PAGE_SIZE
+static char *kbuf;								//define main single buffer for all 
+static char *msg_ptr;
 
-static struct class *maxs_class;
+static char *node_name = "chrdrv";
+
+module_param(node_name, charp, S_IRUGO);
 
 static int max_chr_drv_open(struct inode *inode, struct file *file) {
-	static int counter = 0;
-	char *kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
-
-	file->private_data = kbuf;
-
-	printk( KERN_INFO "Opening device: %s \n", MAX_DEV_NAME);
-	counter++;
-
-	printk( KERN_INFO "counter: %d\n", counter);
-	printk( KERN_INFO "module refcounter: %d\n", module_refcount(THIS_MODULE));
-
+	msg_ptr = kbuf;
+	printk( KERN_INFO "Solution module: Opening device: %s \n", MAX_CLASS_NAME);
 	return 0;
 }
 
 static int max_chr_drv_release(struct inode *inode, struct file *file) {
-	char *kbuf = file->private_data;
-	printk( KERN_INFO "Releasing device: %s \n", MAX_DEV_NAME);
-	if (kbuf) kfree(kbuf);
-	kbuf = NULL;
-	file->private_data = NULL;
-
+	printk( KERN_INFO "Solution module: Releasing device: %s \n", MAX_CLASS_NAME);
 	return 0;
 }
 
 static ssize_t max_chr_drv_read(struct file *file, char __user *buf, size_t lbuf, loff_t *ppos) {
-	char *kbuf = file->private_data;
-	int nbytes = lbuf - copy_to_user(buf, kbuf + *ppos, lbuf);
+	int nbytes = 0;
 
+	sprintf(kbuf, "%d\n", max_chr_drv_major_number);
+
+	if (*msg_ptr == 0) return 0;
+	while ( lbuf && *msg_ptr ) {
+		put_user(*(msg_ptr++), buf++);
+		lbuf--;
+    	nbytes++;
+	}
 	*ppos += nbytes;
-	printk( KERN_INFO "Reading device: %s, bytes readed = %d, ppos = %d\n", MAX_DEV_NAME, nbytes, (int) *ppos );
+	printk( KERN_INFO "Solution module: Reading device: %s, bytes readed = %d, ppos = %d\n", MAX_CLASS_NAME, nbytes, (int) *ppos );
 	return nbytes;
 }
 
 static ssize_t max_chr_drv_write(struct file *file, const char __user *buf, size_t lbuf, loff_t *ppos) {
-	char *kbuf = file->private_data;
 	int nbytes = lbuf - copy_from_user(kbuf + *ppos, buf, lbuf);
 	*ppos += nbytes;
 
-	printk( KERN_INFO "Writing device: %s, bytes writed = %d, ppos = %d\n", MAX_DEV_NAME, nbytes, (int) *ppos );
+	printk( KERN_INFO "Solution module: Writing device: %s, bytes writed = %d, ppos = %d\n", MAX_CLASS_NAME, nbytes, (int) *ppos );
 	return nbytes;
-}
-
-static loff_t max_chr_drv_lseek (struct file *file, loff_t offset, int orig) {
-    loff_t testpos;
-
-    switch (orig) {
-        case SEEK_SET :
-            testpos = offset;
-            break;
-        case SEEK_CUR : 
-            testpos = file->f_pos + offset;
-            break;
-        case SEEK_END : 
-            testpos = KBUF_SIZE + offset;
-            break;
-        default:
-            return -EINVAL;
-            break;
-    }
-
-    if (testpos >= KBUF_SIZE) testpos = KBUF_SIZE - 1;
-    if (testpos < 0) testpos = 0;
-    file->f_pos = testpos;
-
-    printk ( KERN_INFO "Seeking to %ld position\n", (long)testpos);
-
-    return testpos;
 }
 
 static const struct file_operations maxs_cdev_fops = {
@@ -92,41 +67,57 @@ static const struct file_operations maxs_cdev_fops = {
 	.read = max_chr_drv_read,
 	.write = max_chr_drv_write,
 	.open = max_chr_drv_open,
-	.release = max_chr_drv_release,
-    .llseek = max_chr_drv_lseek
+	.release = max_chr_drv_release
 };
 
-static int __init init_module_chrdrv(void) {
-	printk( KERN_INFO "Hello, solution module loaded!\n" );
+static int __init init_mod(void) {
 
-	first = MKDEV (max_chr_drv_major, max_chr_drv_minor);
-	register_chrdev_region (first, count, MAX_DEV_NAME);
+	printk( KERN_INFO "Solution module: Loaded!\n" );
 
-	maxs_cdev = cdev_alloc();
+	// Try to dynamically allocate a major number for the device -- more difficult but worth it
+	max_chr_drv_major_number = register_chrdev(0, node_name, &maxs_cdev_fops);
+	if (max_chr_drv_major_number < 0) {
+		printk(KERN_ALERT "Solution module: failed to register a major number\n");
+		return max_chr_drv_major_number;
+	}
+	
+	printk(KERN_INFO "Solution module: registered major number %d\n", max_chr_drv_major_number);
+	
+	// Register the device class
+	max_chr_drv_class = class_create(THIS_MODULE, MAX_CLASS_NAME);
+	if (IS_ERR(max_chr_drv_class)) {										// Check for error and clean up if there is
+		unregister_chrdev(max_chr_drv_major_number, node_name);
+		printk(KERN_ALERT "Solution module: Failed to register device class\n");
+		return PTR_ERR(max_chr_drv_class);									// Correct way to return an error on a pointer
+	}
+	printk(KERN_INFO "Solution module: device class registered correctly\n");
 
-	cdev_init (maxs_cdev, &maxs_cdev_fops);
-	cdev_add (maxs_cdev, first, count);
+	// Register the device driver
+	max_chr_drv_device = device_create(max_chr_drv_class, NULL, MKDEV(max_chr_drv_major_number, 0), NULL, node_name);
+	if (IS_ERR(max_chr_drv_device)) {										// Clean up if there is an error
+		class_destroy(max_chr_drv_class); 									// Repeated code but the alternative is goto statements
+		unregister_chrdev(max_chr_drv_major_number, node_name);
+		printk(KERN_ALERT "Solution module: Failed to create the device\n");
+		return PTR_ERR(max_chr_drv_device);
+	}
+	printk(KERN_INFO "Solution module: device class created correctly\n"); // Made it! device was initialized
 
-    maxs_class = class_create (THIS_MODULE, "maxs_class");
-    device_create (maxs_class, NULL, first, "%s", "chrdrv");
-
-    printk ( KERN_INFO "Created device class :%s\n", MAX_DEV_NAME);
+	kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
 
 	return 0;
 }
 
-static void __exit exit_module_chrdrv(void) {
+static void __exit exit_mod(void) {
 
-    device_destroy (maxs_class, first);
-    class_destroy (maxs_class);
+	if (kbuf) kfree(kbuf);
 
-	if (maxs_cdev)
-		cdev_del (maxs_cdev);
-	unregister_chrdev_region(first, count);
-	printk( KERN_INFO "Hello, solution module leaved!\n" );
+	device_destroy(max_chr_drv_class, MKDEV(max_chr_drv_major_number, 0));	// remove the device
+	class_unregister(max_chr_drv_class);									// unregister the device class
+	class_destroy(max_chr_drv_class);										// remove the device class
+	unregister_chrdev(max_chr_drv_major_number, node_name);			// unregister the major number
+
+	printk( KERN_INFO "Solution module: Leaved!\n" );
 }
 
-module_init(init_module_chrdrv);
-module_exit(exit_module_chrdrv);
-
-MODULE_LICENSE("GPL");
+module_init(init_mod);
+module_exit(exit_mod);
